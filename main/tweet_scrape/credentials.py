920b157fbd37389cf60bc9e8d51ab93184103f4c
James Nelson <43624099+jnelly79@users.noreply.github.com>
2020-12-09 12:34:34 -0700

    create credentials.py

---
+++ main/tweet_scrape/credentials.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+cred = {
+        "consumer_key": "your consumer key",
+        "consumer_secret": "your consumer secret",
+        "access_token": "your access token",
+        "access_token_secret": "your access token secret"
+        }
